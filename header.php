befd7f4c60e371afe3b842457474d7ea4132ad8c
ppilovee01 <ppilovee01@gmail.com>
2026-05-27 00:42:38 +0700

    แก้ไขให้ออฟไลน์ได้

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ระบบฐานข้อมูลพรรณไม้แห้ง</title>
-    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="js/tailwindcss.js"></script>
     <link href="https://fonts.googleapis.com/css2?family=Sarabun:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <style>body { font-family: 'Sarabun', sans-serif; }</style>
 </head>
